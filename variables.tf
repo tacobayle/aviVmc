53b133ba328d8b19fa09c0a1ce70ad9c58995f26
tacobayle <nicolas.bayle@gmail.com>
2021-01-26 17:35:21 +0000

    20210126

--- variables.tf
+++ variables.tf
@@ -55,7 +55,7 @@ variable "ansible" {
   default = {
     version = "2.9.12"
     aviConfigureUrl = "https://github.com/tacobayle/aviConfigure"
-    aviConfigureTag = "v3.84"
+    aviConfigureTag = "v3.86"
     opencartInstallUrl = "https://github.com/tacobayle/ansibleOpencartInstall"
     opencartInstallTag = "v1.21"
     directory = "ansible"
