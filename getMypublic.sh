d6f551d6b24621a65f53a6464c74b54720776cd4
tacobayle <nicolas.bayle@gmail.com>
2021-03-13 20:22:43 +0000

    20210313

--- getMypublic.sh
+++ getMypublic.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ifPrimary=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
-ip=$(ip -f inet addr show $ifPrimary | awk '/inet / {print $2}' | awk -F/ '{print $1}
-')
+ip=$(ip -f inet addr show $ifPrimary | awk '/inet / {print $2}' | awk -F/ '{print $1}')
 myPublicIP=$(curl ifconfig.me)
 echo "{\"my_private_ip\": \"$ip\", \"my_public_ip\": \"$myPublicIP\"}" | tee ip.json
